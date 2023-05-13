63844b90cbb1f7d48260ae6252438ff10cfd4798
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-05-13 15:53:52 +0200

    nuovo

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import Joi from "joi";
+import {getAll, getOneById, create, updateById, deleteById} from './controllers/planets.js'
 
 const app = express();
 const port = 3000;
@@ -9,68 +9,15 @@ const port = 3000;
 app.use(morgan('dev'));
 app.use(express.json());
 
-type Planet = {
-  id: number,
-  name: string,
-};
+app.get('/api/planets', getAll)
 
-type Planets = Planet[];
+app.get('/api/planets/:id', getOneById)
 
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
+app.post('/api/planets', create)
 
-const planetSchema = Joi.object({
-  id: Joi.number().integer().required(),
-  name: Joi.string().required()
-})
+app.put('/api/planets/:id', updateById)
 
-app.get('/api/planets', (req, res) => {
-  res.status(200).json(planets)
-})
-
-app.get('/api/planets/:id', (req, res) => {
-  const {id} = req.params;
-  const planet = planets.find((p) => p.id === Number(id))
-
-  res.status(200).json(planet)
-})
-
-app.post('/api/planets', (req, res) => {
-  const {id, name} = req.body;
-  const newPlanet: Planet = {id, name};
-  const validateNewPlanet = planetSchema.validate(newPlanet)
-
-  if(validateNewPlanet.error) {
-    return res.status(400).json({ msg: validateNewPlanet.error.details[0].message })
-  } else {
-    planets = [...planets, newPlanet]
-
-    res.status(201).json({ msg: 'The planet was created'})
-  }
-})
-
-app.put('/api/planets/:id', (req, res) => {
-  const {id} = req.params;
-  const {name} = req.body;
-  planets = planets.map((p) => (p.id === Number(id) ? {...p, name} : p));
-
-  res.status(200).json({ msg: 'The planet was updated'})
-})
-
-app.delete('/api/planets/:id', (req, res) => {
-  const {id} = req.params;
-  planets = planets.filter((p) => p.id !== Number(id))
-
-res.status(200).json({ msg: 'The planet was deleted' })
-})
+app.delete('/api/planets/:id', deleteById)
 
 app.listen(port, () => {
   console.log(`Example app listening on port: http://localhost:${port}`);
